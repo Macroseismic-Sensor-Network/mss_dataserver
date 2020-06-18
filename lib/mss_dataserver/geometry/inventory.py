3419c8f8f5fc3ed9faddcf966bdedc8ef35b6dcd
Stefan Mertl <info@mertl-research.at>
2020-06-18 15:22:26 +0200

    Added the id as a search option.

--- lib/mss_dataserver/geometry/inventory.py
+++ lib/mss_dataserver/geometry/inventory.py
@@ -547,7 +547,7 @@ class Inventory(object):
         '''
         ret_station = list(itertools.chain.from_iterable([x.stations for x in self.networks]))
 
-        valid_keys = ['name', 'network', 'location']
+        valid_keys = ['id', 'name', 'network', 'location']
 
         for cur_key, cur_value in kwargs.items():
             if cur_key in valid_keys:
