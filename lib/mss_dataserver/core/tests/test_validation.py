4086c6cae9c06c5da9024ffe7b8f87b0d2b0e7d2
Stefan Mertl <info@mertl-research.at>
2020-12-19 16:24:54 +0100

    Added tests for the event validation.

---
+++ lib/mss_dataserver/core/tests/test_validation.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+ # LICENSE
+ #
+ # This file is part of mss_dataserver.
+ # 
+ # If you use mss_dataserver in any program or publication, please inform and
+ # acknowledge its authors.
+ # 
+ # mss_dataserver is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ # 
+ # mss_dataserver is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ # 
+ # You should have received a copy of the GNU General Public License
+ # along with mss_dataserver. If not, see <http://www.gnu.org/licenses/>.
+ #
+ # Copyright 2019 Stefan Mertl
+##############################################################################
+
+'''
+@author: Stefan Mertl
+'''
+import unittest
+import logging
+import threading
+
+import obspy.core
+from obspy.core.utcdatetime import UTCDateTime
+
+import mss_dataserver
+import mss_dataserver.core.validation as validation
+
+
+class ValidationTestCase(unittest.TestCase):
+    """
+    Test suite.
+    """
+    @classmethod
+    def setUpClass(cls):
+        # Configure the logger.
+        cls.logger = logging.getLogger('mss_dataserver')
+        cls.logger.addHandler(mss_dataserver.get_logger_handler(log_level = 'DEBUG'))
+
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def test_event_validation(self):
+        ''' Test event validation.
+        '''
+        # Test a complete event.
+        event = validation.Event(id = 1,
+                                 start_time = UTCDateTime('2020-01-01T01:00:00').isoformat(),
+                                 end_time = UTCDateTime('2020-01-01T01:01:00').isoformat(),
+                                 description = 'Description',
+                                 comment = 'Comment',
+                                 max_pgv = 0.001,
+                                 state = None)
+
+        self.assertIsInstance(event.dict(), dict)
+
+        event = validation.Event(id = None,
+                                 start_time = UTCDateTime('2020-01-01T01:00:00').isoformat(),
+                                 end_time = UTCDateTime('2020-01-01T01:01:00').isoformat(),
+                                 description = 'Description',
+                                 comment = 'Comment',
+                                 max_pgv = 0.001,
+                                 state = 'created')
+
+        self.assertIsInstance(event.dict(), dict)
+
+        event = validation.Event(id = None,
+                                 start_time = UTCDateTime('2020-01-01T01:00:00').isoformat(),
+                                 end_time = UTCDateTime('2020-01-01T01:01:00').isoformat(),
+                                 description = None,
+                                 comment = None,
+                                 max_pgv = 0.001,
+                                 state = None)
+
+        self.assertIsInstance(event.dict(), dict)
+        print(event.dict())
+
+def suite():
+    return unittest.makeSuite(ValidationTestCase, 'test')
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
