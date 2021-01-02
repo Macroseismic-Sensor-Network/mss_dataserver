88767bf82d987fd4a70c70986e5783d56187a918
Stefan Mertl <info@mertl-research.at>
2021-01-02 13:11:38 +0100

    Added a general file encoder.

--- lib/mss_dataserver/core/json_util.py
+++ lib/mss_dataserver/core/json_util.py
@@ -26,6 +26,7 @@
 import json
 import logging
 
+import numpy as np
 import obspy
 
 import mss_dataserver.core.util as util
@@ -53,6 +54,81 @@ class FileContainer(object):
 
     def __init__(self, data = {}):
         self.data = data
+
+
+class GeneralFileEncoder(json.JSONEncoder):
+    ''' A JSON encoder for the serialization of general data.
+    '''
+    version = util.Version('1.0.0')
+
+    def __init__(self, **kwarg):
+        json.JSONEncoder.__init__(self, **kwarg)
+
+        # The logger.
+        loggerName = __name__ + "." + self.__class__.__name__
+        self.logger = logging.getLogger(loggerName)
+
+        # File format settings.
+        self.indent = 4
+        self.sort_keys = True
+
+
+    def default(self, obj):
+        ''' Convert pSysmon project objects to a dictionary.
+        '''
+        obj_class = obj.__class__.__name__
+        base_class = [x.__name__ for x in obj.__class__.__bases__]
+
+        #self.logger.debug('obj_class: %s.', obj_class)
+        if obj_class == 'FileContainer':
+            d = self.convert_filecontainer(obj)
+        elif obj_class == 'Version':
+            d = self.convert_version(obj)
+        elif obj_class == 'UTCDateTime':
+            d = self.convert_utcdatetime(obj)
+        elif obj_class == 'ndarray':
+            d = self.convert_np_ndarray(obj)
+        elif obj_class == 'type':
+            d = {}
+        else:
+            d = {'ERROR': 'MISSING CONVERTER for obj_class {obj_class} with base_class {base_class}'.format(obj_class = str(obj_class),
+                                                                                                            base_class = str(base_class))}
+
+        # Add the class and module information to the dictionary.
+        if obj_class != 'FileContainer':
+            try:
+                module = obj.__module__
+            except Exception:
+                module = obj.__class__.__module__
+            tmp = {'__baseclass__': base_class,
+                   '__class__': obj.__class__.__name__,
+                   '__module__': module}
+            d.update(tmp)
+
+        self.logger.debug('d: %s', d)
+        return d
+
+
+    def convert_filecontainer(self, obj):
+        d = obj.data
+        file_meta = {'file_version': self.version,
+                     'save_date': obspy.UTCDateTime()}
+        d['file_meta'] = file_meta
+        return d
+
+
+    def convert_utcdatetime(self, obj):
+        return {'utcdatetime': obj.isoformat()}
+
+
+    def convert_version(self, obj):
+        return {'version': str(obj)}
+
+
+    def convert_np_ndarray(self, obj):
+        return {'data': obj.tolist()}
+
+
 
 
 class SupplementDetectionDataEncoder(json.JSONEncoder):
@@ -87,7 +163,7 @@ class SupplementDetectionDataEncoder(json.JSONEncoder):
         elif obj_class == 'Version':
             d = self.convert_version(obj)
         elif obj_class == 'ndarray':
-            d = self.convert_numpy_array(obj)
+            d = self.convert_np_ndarray(obj)
         elif 'Station' in base_class:
             d = self.convert_station(obj)
         else:
@@ -120,10 +196,68 @@ class SupplementDetectionDataEncoder(json.JSONEncoder):
     def convert_version(self, obj):
         return {'version': str(obj)}
 
-    def convert_numpy_array(self, obj):
+    def convert_np_ndarray(self, obj):
         return {'data': obj.tolist()}
 
     def convert_station(self, obj):
         attr = ['name', 'location', 'network']
         d = object_to_dict(obj, attr)
         return d
+
+
+class SupplementDetectionDataDecoder(json.JSONDecoder):
+    version = util.Version('1.0.0')
+
+    def __init__(self, **kwarg):
+        json.JSONDecoder.__init__(self, object_hook = self.convert_dict)
+
+
+    def convert_dict(self, d):
+        #print "Converting dict: %s." % str(d)
+
+        if '__class__' in d:
+            class_name = d.pop('__class__')
+            module_name = d.pop('__module__')
+            base_class = d.pop('__baseclass__')
+
+            if class_name == 'Version':
+                inst = self.convert_version(d)
+            elif class_name == 'UTCDateTime':
+                inst = self.convert_utcdatetime(d)
+            elif class_name == 'ndarray':
+                inst = self.convert_np_array(d)
+            else:
+                inst = {'ERROR': 'MISSING CONVERTER'}
+
+        else:
+            inst = d
+
+        return inst
+
+
+    def decode_hinted_tuple(self, item):
+        if isinstance(item, dict):
+            if '__tuple__' in item:
+                return tuple(item['items'])
+        elif isinstance(item, list):
+                return [self.decode_hinted_tuple(x) for x in item]
+        else:
+            return item
+
+
+    def convert_version(self, d):
+        inst = util.Version(d['version'])
+        return inst
+
+
+    def convert_utcdatetime(self, d):
+        inst = obspy.UTCDateTime(d['utcdatetime'])
+        return inst
+
+
+    def convert_np_array(self, d):
+        inst = np.array(d['data'])
+        return inst
+
+
+
