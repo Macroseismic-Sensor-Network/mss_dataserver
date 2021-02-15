f0b3a2309349b3e8c6f74337777b1d2f5b1224ce
Stefan Mertl <info@mertl-research.at>
2021-02-15 16:22:12 +0100

    Added models for the Websocket messages.

--- lib/mss_dataserver/core/validation.py
+++ lib/mss_dataserver/core/validation.py
@@ -23,12 +23,15 @@
  # Copyright 2020 Stefan Mertl
 ##############################################################################
 
+import enum
+
 import pydantic
 from pydantic import (
-    PositiveInt,
-    PositiveFloat,
     confloat,
     constr,
+    PositiveInt,
+    PositiveFloat,
+    validator
 )
 
 from typing import (
@@ -50,3 +53,66 @@ class Event(pydantic.BaseModel):
     description: Optional[constr(max_length=255)] = None
     comment: Optional[constr(max_length=255)] = None
     state: Optional[constr(max_length=20)] = None
+
+
+class MsgClassEnum(str, enum.Enum):
+    control = 'control'
+    soh = 'soh'
+    data = 'data'
+
+
+class MsgControlIdEnum(str, enum.Enum):
+    mode = 'mode'
+
+
+class MsgRequestIdEnum(str, enum.Enum):
+    event_supplement = 'event_supplement'
+
+
+class MsgSohIdEnum(str, enum.Enum):
+    connection = 'connection'
+    server_state = 'server_state'
+
+
+class MsgDataIdEnum(str, enum.Enum):
+    current_pgv = 'current_pgv'
+    pgv = 'pgv'
+    pgv_archive = 'pgv_archive'
+    detection_result = 'detection_result'
+    event_data = 'event_data'
+    event_archive = 'event_archive'
+    event_warning = 'event_warning'
+    key_data = 'key_data'
+    supplement = 'supplement'
+
+
+class WSMessageHeader(pydantic.BaseModel):
+    msg_class: MsgClassEnum
+    msg_id: Union[MsgControlIdEnum,
+                  MsgDataIdEnum,
+                  MsgSohIdEnum,
+                  MsgRequestIdEnum]
+    server_time: constr(regex=r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{6})?')
+
+    @validator('msg_id')
+    def check_msg_id(cls, v, values):
+        ''' Check if the msg_id is in the allowed values for the related msg_class.
+        '''
+        msg_class = values['msg_class']
+        class_enum = None
+
+        if msg_class is MsgClassEnum.control:
+            class_enum = MsgControlIdEnum
+        elif msg_class is MsgClassEnum.soh:
+            class_enum = MsgSohIdEnum
+        elif msg_class is MsgClassEnum.data:
+            class_enum = MsgDataIdEnum
+
+        if v not in class_enum.__members__.values():
+            raise ValueError('The msg_id "{msg_id}" is not allowed.'.format(msg_id = v))
+        return v
+
+
+class WSMessage(pydantic.BaseModel):
+    header: WSMessageHeader
+    payload: dict
