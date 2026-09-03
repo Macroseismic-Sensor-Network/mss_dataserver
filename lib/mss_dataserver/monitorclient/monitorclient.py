Fixed the computation of the last_year_timespan.

lib/mss_dataserver/monitorclient/monitorclient.py
@@ -279,11 +279,12 @@ class MonitorClient(easyseedlink.EasySeedLinkClient):
         self.archive_limits = {'start': processing_start,
                                'recent_timespan': event_archive_timespan}
         if load_events:
-            full_timespan = obspy.UTCDateTime() - processing_start
-            full_timespan = np.ceil(full_timespan / 3600)
-            last_year = obspy.UTCDateTime() - (365 * 24 * 3600)
-            last_year = np.ceil(last_year / 3600)
-            self.load_archive_catalogs(hours = last_year)
+            #full_timespan = obspy.UTCDateTime() - processing_start
+            #full_timespan = np.ceil(full_timespan / 3600)
+            now = obspy.UTCDateTime()
+            last_year_timespan = now - (now - (365 * 24 * 3600))
+            last_year_timespan = np.ceil(last_year_timespan / 3600)
+            self.load_archive_catalogs(hours = last_year_timespan)
 
     def reset(self, reload_events = True):
         ''' Reset the monitorclient to an initial state.
@@ -299,12 +300,13 @@ class MonitorClient(easyseedlink.EasySeedLinkClient):
 
         if reload_events:
             self.project.event_library.clear()
-            processing_start = self.archive_limits['start']
-            full_timespan = obspy.UTCDateTime() - processing_start
-            full_timespan = np.ceil(full_timespan / 3600)
-            last_year = obspy.UTCDateTime() - (365 * 24 * 3600)
-            last_year = np.ceil(last_year / 3600)
-            self.load_archive_catalogs(hours = last_year)
+            #processing_start = self.archive_limits['start']
+            #full_timespan = obspy.UTCDateTime() - processing_start
+            #full_timespan = np.ceil(full_timespan / 3600)
+            now = obspy.UTCDateTime()
+            last_year_timespan = now - (now - (365 * 24 * 3600))
+            last_year_timespan = np.ceil(last_year_timespan / 3600)
+            self.load_archive_catalogs(hours = last_year_timespan)
         self.detector.reset()
 
 